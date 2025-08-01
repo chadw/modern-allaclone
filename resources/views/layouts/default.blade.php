4cecb2bca798f49c25b99d1a7a8986a3ee0362c9
chadw <cweaber@gmail.com>
2025-08-01 12:28:27 -0400

    added footer to main layout and errors layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="dark">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,31 +10,46 @@
     <base href="{{ url('/') }}/">
     @vite(['resources/css/app.css'])
 </head>
+
 <body class="min-h-screen flex flex-col">
     <div class="grow py-5 bg-base-300">
         <div class="container mx-auto px-4">
             @include('layouts.partials.navbar')
-            <div class="flex flex-col min-w-0 break-words bg-base-200 w-full mb-6 rounded-lg min-h-lvh">
+            <div class="flex flex-col min-w-0 break-words bg-base-200 w-full mb-6 rounded-t-lg min-h-lvh">
                 <div class="p-10 h-full">
                     <x-h1>@yield('title')</x-h1>
                     @yield('content')
                 </div>
             </div>
+            <div class="w-full relative -mt-6 z-10 flex justify-center">
+                <div
+                    class="bg-neutral border border-base-100 text-sm text-gray-400 px-6 py-4 rounded-b-xl shadow-md w-full flex justify-between items-center">
+                    <div>
+                        {{ config('app.name') }}
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <span>Loot the source on</span>
+                        <a href="https://github.com/chadw/modern-allaclone" target="_blank"
+                            class="flex items-center gap-1 link-accent link-hover">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="w-4 h-4">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path
+                                    d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                            </svg>
+                            <span>GitHub</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     @vite(['resources/js/app.js'])
-    <div
-        x-data="{ show: false }"
-        x-init="window.addEventListener('scroll', () => show = window.scrollY > 200)"
-        x-show="show"
-        x-transition
-        class="fixed bottom-6 right-6 z-50"
-    >
-        <button
-            @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
-            class="bg-base-200 text-base-content hover:bg-base-300 p-3 rounded-full shadow-lg"
-            aria-label="Back to top"
-        >
+    <div x-data="{ show: false }" x-init="window.addEventListener('scroll', () => show = window.scrollY > 200)" x-show="show" x-transition
+        class="fixed bottom-6 right-6 z-50">
+        <button @click="window.scrollTo({ top: 0, behavior: 'smooth' })"
+            class="bg-base-200 text-base-content hover:bg-base-300 p-3 rounded-full shadow-lg" aria-label="Back to top">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
@@ -46,4 +62,5 @@
         style="position: absolute; display: none; top: 0; left: 0">
     </div>
 </body>
+
 </html>
--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -10,7 +10,7 @@
     @vite(['resources/css/app.css'])
 </head>
 <body class="flex items-center justify-center min-h-screen bg-base-100">
-    <main class="relative max-w-2xl w-full p-10 bg-base-200 rounded-xl shadow-sm flex flex-col items-center">
+    <main class="relative max-w-2xl w-full p-10 bg-base-200 rounded-t-xl shadow-sm flex flex-col items-center">
         <div class="absolute top-0 left-1/2 -translate-y-1/2 -translate-x-1/2">
             <img src="{{ asset('img/eqemu.png') }}"
                 title="{{ config('app.name') }}"
@@ -21,6 +21,28 @@
         </div>
         <div class="w-full mt-6">
             @yield('content')
+        </div>
+        <div class="w-full absolute -bottom-13 z-10 flex justify-center">
+            <div
+                class="bg-neutral text-sm text-gray-400 px-6 py-4 rounded-b-xl shadow-md w-full flex justify-between items-center">
+                <div>
+                    {{ config('app.name') }}
+                </div>
+                <div class="flex items-center gap-1">
+                    <span>Loot the source on</span>
+                    <a href="https://github.com/chadw/modern-allaclone" target="_blank"
+                        class="flex items-center gap-1 link-accent link-hover">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="w-4 h-4">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                        </svg>
+                        <span>GitHub</span>
+                    </a>
+                </div>
+            </div>
         </div>
     </main>
 </body>

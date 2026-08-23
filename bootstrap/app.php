d6c8f02af09bb67dd09d359eb57d885dfbaafcfc
chadw <cweaber@gmail.com>
2026-08-23 12:26:57 -0400

    remove dupe task middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Http\Middleware\SecurityHeaders;
 use App\Http\Middleware\TasksEnabled;
 use App\Http\Middleware\TradeskillPlannerEnabled;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\SecurityHeaders;
-use App\Http\Middleware\TasksEnabled;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
